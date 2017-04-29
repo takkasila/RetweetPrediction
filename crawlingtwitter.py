a82ed31409e121593a18b2d2c7c0a77e12073b6c
acrobat3 <Legend_of_oh@hotmail.com>
2017-04-29 20:09:56 +0700

    added crawling

---
+++ crawlingtwitter.py
@@ -0,0 +1,53 @@
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy import API
+from tweepy import Cursor
+import json
+
+consumer_key = 'B2belwJ2uBpWB8LJYdxkeLHH4'
+consumer_secret = 'bbHVcfTpxaV78fGXPWLnfi3vkbi3CTqqmhL9B1CeHWjTdrFE2r'
+access_token = '858235205471141888-WyasLMlGkn10yGNAYKNj9SQwL2YHdrm'
+access_token_secret = 'fviwd7yotvuDpGazFHj4W7Blbmv6zsJdamOGjJ8d54uN8'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+dic = {}
+
+api = API(auth)
+queue = []
+count = 0
+
+def readTweet(seed):
+	tweets = api.user_timeline(seed, count = 40, include_rts = True)
+	dic[seed] = {}
+	for tweet in tweets:
+		try:
+			if tweet.retweeted_status.user.id in dic[seed]:
+				dic[seed][tweet.retweeted_status.user.id] += 1
+			else:
+				dic[seed][tweet.retweeted_status.user.id] = 1
+		except:
+			pass
+
+
+if __name__ == '__main__':
+	seed_user = api.get_user('waiiwall')
+
+	queue.append(seed_user.id)
+	while count < 10:
+		seed = queue.pop(0)
+		try:
+			readTweet(seed)
+		except tweepy.TweepError:
+			time.sleep(60 * 15)
+			readTweet(seed)
+		except:
+			pass
+		new_seed = [int(k) for k in dic[seed]]
+		queue = queue+new_seed
+		count+=1
+	with open("retweet.json", "w") as fp:
+		json.dump(dic, fp)
