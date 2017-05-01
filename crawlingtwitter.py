c502b89b66ffb7ab3d24a4f18aaa6f1f4002257d
takkasila <takkasila@outlook.com>
2017-05-01 22:02:58 +0700

    Change JSON format

--- crawlingtwitter.py
+++ crawlingtwitter.py
@@ -1,4 +1,5 @@
 #Import the necessary methods from tweepy library
+import tweepy
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
@@ -6,6 +7,7 @@ from tweepy import API
 from tweepy import Cursor
 import datetime, time
 import json
+import collections
 
 consumer_key = 'B2belwJ2uBpWB8LJYdxkeLHH4'
 consumer_secret = 'bbHVcfTpxaV78fGXPWLnfi3vkbi3CTqqmhL9B1CeHWjTdrFE2r'
@@ -15,44 +17,61 @@ access_token_secret = 'fviwd7yotvuDpGazFHj4W7Blbmv6zsJdamOGjJ8d54uN8'
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
-dic = {}
+dict = collections.OrderedDict()
 
 api = API(auth)
 queue = []
-count = 0
+search_count = 0
+max_search_node = 2
+total_node_count = 0 
 
 def readTweet(seed):
 	tweets = api.user_timeline(seed, count = 40, include_rts = True)
-	dic[seed] = {}
+	dict[seed] = collections.OrderedDict()
+	dict[seed]['uid'] = seed
+	dict[seed]['edge'] = collections.OrderedDict()
 	for tweet in tweets:
 		try:
-			day = (datetime.datetime.now()-tweet.retweeted_status.created_at).days
-			sec = (datetime.datetime.now()-tweet.retweeted_status.created_at).seconds
-			if tweet.retweeted_status.user.id in dic[seed]:
-				dic[seed][tweet.retweeted_status.user.id][0] += 1
-				dic[seed][tweet.retweeted_status.user.id][1].append((day, sec))
+			epochTime = int((tweet.retweeted_status.created_at -  datetime.datetime(1970,1,1)).total_seconds())
+			# Already pushed in dictt
+			if tweet.retweeted_status.user.id in dict[seed]['edge']:
+				dict[seed]['edge'][tweet.retweeted_status.user.id]['count'] += 1
+				dict[seed]['edge'][tweet.retweeted_status.user.id]['time_stamp'][
+					dict[seed]['edge'][tweet.retweeted_status.user.id]['count']-1
+				] = epochTime
+			# Newly insert
 			else:
-				dic[seed][tweet.retweeted_status.user.id] = (1, [(day, sec)])
-		except:
+				dict[seed]['edge'][tweet.retweeted_status.user.id] = collections.OrderedDict()
+				dict[seed]['edge'][tweet.retweeted_status.user.id]['uid'] = tweet.retweeted_status.user.id
+				dict[seed]['edge'][tweet.retweeted_status.user.id]['count'] = 1
+				dict[seed]['edge'][tweet.retweeted_status.user.id]['time_stamp'] = collections.OrderedDict()
+				dict[seed]['edge'][tweet.retweeted_status.user.id]['time_stamp']['0'] = epochTime
+				queue.append(tweet.retweeted_status.user.id)
+				global total_node_count
+				total_node_count += 1
+		except Exception as e:
 			pass
-
+	print "Done crawling: ", seed
 
 if __name__ == '__main__':
 	seed_user = api.get_user('waiiwall')
-	max_node = 2000
 	queue.append(seed_user.id)
-	while count < max_node:
-		print("{}/{}".format(count, max_node))
-		seed = queue.pop(0)
+	while search_count < max_search_node:
+		print("{}/{}".format(search_count, max_search_node))
 		try:
+			print "Try reading."
+			seed = queue.pop(0)
+			total_node_count += 1
 			readTweet(seed)
 		except tweepy.TweepError:
+			print "Waiting 15 mins..."
 			time.sleep(60 * 15)
 			readTweet(seed)
-		except:
-			pass
-		new_seed = [int(k) for k in dic[seed]]
-		queue = queue+new_seed
-		count+=1
-	with open("retweet.json", "w") as fp:
-		json.dump(dic, fp)
+		except Exception as e:
+			print "Error"
+			print e
+		
+		search_count+=1
+		print("Total node: {}".format(total_node_count))
+	with open("retweet3.json", "w") as fp:
+		json.dump(dict, fp)
---
+++ reading_data_example.py
@@ -0,0 +1,12 @@
+import json
+import pprint
+
+dataFile = open('retweet.json')
+
+data = json.load(dataFile)
+
+for node_id in data:
+	print "At node: ", data[node_id]['uid']
+	for target_node in data[node_id]['edge']:
+		print("Visit: {} for: {} times".format(data[node_id]['edge'][target_node]['uid']
+												, data[node_id]['edge'][target_node]['count']))
